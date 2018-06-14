14a708a93eeacef1a1d62df335b5113b719910d0
Unknown <yaejiyoon@gmail.com>
2018-06-14 11:49:06 +0900

    DTO, DAO 작성 완료

--- src/kh/web/dao/MessageDAO.java
+++ src/kh/web/dao/MessageDAO.java
@@ -1,5 +1,56 @@
 package kh.web.dao;
 
-public class MessageDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+import kh.web.dto.MessageDTO;
+
+public class MessageDAO {
+	private Connection getConnetion()  throws Exception {
+		Context context = new InitialContext();
+		DataSource ds = (DataSource)context.lookup("java:/comp/env/oracle");
+		return ds.getConnection();
+	}
+	
+	public int insertMessage (String name, String message) throws Exception {
+		Connection con = this.getConnetion();
+		String sql = "insert into message values (msg_seq.nextval,?,?)";
+		PreparedStatement pstat = con.prepareStatement(sql);
+		pstat.setString(1, name);
+		pstat.setString(2, message);
+		int result = pstat.executeUpdate();
+		con.commit();
+		pstat.close();
+		con.close();
+		return result;
+
+	}
+	
+	public List<MessageDTO> getAllData() throws Exception {
+		Connection con = this.getConnetion();
+		String sql = "select * from message";
+		PreparedStatement pstat = con.prepareStatement(sql);
+		ResultSet rs = pstat.executeQuery();
+		
+		List<MessageDTO> result = new ArrayList<>();
+		while(rs.next()) {
+			MessageDTO dto = new MessageDTO();
+			dto.setSeq(rs.getInt("seq"));
+			dto.setName(rs.getString("name"));
+			dto.setMessage(rs.getString("message"));
+			result.add(dto);
+		}
+		pstat.close();
+		con.close();
+		return result;
+		
+	}
+	
 }
--- src/kh/web/dto/MessageDTO.java
+++ src/kh/web/dto/MessageDTO.java
@@ -1,5 +1,44 @@
 package kh.web.dto;
 
 public class MessageDTO {
+	private int seq;
+	private String name;
+	private String message;
+	
+	public MessageDTO() {}
 
+	public MessageDTO(int seq, String name, String message) {
+		super();
+		this.seq = seq;
+		this.name = name;
+		this.message = message;
+	}
+
+	public int getSeq() {
+		return seq;
+	}
+
+	public void setSeq(int seq) {
+		this.seq = seq;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+	
+	
 }
